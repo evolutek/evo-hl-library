5355b2d64908b89c7756a80527d2c3a978abe389
Kolte200 <celian.aldehuelo@gmail.com>
2026-03-28 13:05:53 +0100

    chore: Move scheduler from AI PR of evo-hl-omnissiah

---
+++ src/evo_lib/scheduler.py
@@ -0,0 +1,40 @@
+import sched
+import time
+from typing import Callable
+
+from evo_lib.executor import BaseExecutor
+
+
+class SchedulerExecutor(BaseExecutor):
+    def __init__(self, scheduler: "Scheduler", priority: int):
+        self.scheduler: "Scheduler" = scheduler
+        self.priority: int = priority
+
+    def exec(self, callback: Callable, args: tuple, kwargs: dict[str,]) -> None:
+        self.scheduler.schedule_now(self.priority, callback, args, kwargs)
+
+
+class Scheduler:
+    def __init__(self):
+        self.s = sched.scheduler(time.time, time.sleep)
+
+    def schedule_now(self, priority: int, action: Callable, args: tuple, kwargs: dict[str,]) -> sched.Event:
+        self.s.enter(0, priority, action, argument = args, kwargs = kwargs)
+
+    def schedule_after(self, delay: float, priority: int, action: Callable, args: tuple, kwargs: dict[str,]) -> sched.Event:
+        self.s.enter(delay, priority, action, argument = args, kwargs = kwargs)
+
+    def schedule_at(self, timepoint: float, priority: int, action: Callable, args: tuple, kwargs: dict[str,]) -> sched.Event:
+        self.s.enterabs(timepoint, priority, action, argument = args, kwargs = kwargs)
+
+    def cancel(self, scheduled: sched.Event) -> None:
+        self.s.cancel(scheduled)
+
+    def run(self) -> None:
+        self.s.run(blocking = True)
+
+    def handle(self) -> float:
+        return self.s.run(blocking = False)
+
+    def get_executor(self, priority: int) -> SchedulerExecutor:
+        return SchedulerExecutor(self, priority)
