79056df41be26204c68e57b0c9265fd4e8f670bc
LeCrabe <barcelo.mathieu@gmail.com>
2026-04-06 15:04:05 +0200

    feat: add generic graph execution engine

---
+++ src/evo_lib/graph/nodes/flow.py
@@ -0,0 +1,50 @@
+"""Built-in flow control nodes: Entry, IfElse."""
+
+from evo_lib.graph.graph import Node, NodeDefinition, Graph, FlowOutput, ValueInput
+from evo_lib.argtypes import ArgTypes
+
+
+class EntryNode(Node):
+    def __init__(self, definition: NodeDefinition, name: str, graph: Graph):
+        super().__init__(definition, name, graph)
+
+    def run(self) -> None:
+        output = self.get_flow_output("flow")
+        if output is not None:
+            output.run()
+
+
+class EntryNodeDefinition(NodeDefinition[EntryNode]):
+    def __init__(self):
+        super().__init__(EntryNode, "entry", "Entry")
+        self.add_flow_output("flow")
+
+
+class IfElseNode(Node):
+    def __init__(self, definition: NodeDefinition, name: str, graph: Graph):
+        super().__init__(definition, name, graph)
+
+    def run(self) -> None:
+        condition = self.get_value_input("condition")
+        true_output = self.get_flow_output("true")
+        false_output = self.get_flow_output("false")
+
+        if condition is not None and condition.get_value():
+            if true_output:
+                true_output.run()
+            if false_output:
+                false_output.ignore()
+        else:
+            if true_output:
+                true_output.ignore()
+            if false_output:
+                false_output.run()
+
+
+class IfElseNodeDefinition(NodeDefinition[IfElseNode]):
+    def __init__(self):
+        super().__init__(IfElseNode, "if", "If")
+        self.add_flow_input("flow")
+        self.add_flow_output("true")
+        self.add_flow_output("false")
+        self.add_value_input("condition", ArgTypes.Bool, False)
