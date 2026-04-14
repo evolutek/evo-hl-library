f70f6b48f0ecdd8cc1474de59fdd9544b9b4c828
Kolte200 <celian.aldehuelo@gmail.com>
2026-04-14 20:34:14 +0200

    update: WIP serial pilot

--- src/evo_lib/interfaces/pilot.py
+++ src/evo_lib/interfaces/pilot.py
@@ -5,6 +5,8 @@ from dataclasses import dataclass
 from enum import StrEnum
 from typing import TYPE_CHECKING
 
+from evo_lib.argtypes import ArgTypes
+from evo_lib.driver_definition import DriverCommands
 from evo_lib.event import Event
 from evo_lib.peripheral import Placable
 from evo_lib.types.pose import Pose2D
@@ -54,17 +56,22 @@ class HolonomicPilotWaypoint(DifferentialPilotWaypoint):
 
 
 class Pilot(Placable):
+    commands = DriverCommands()
+
     @abstractmethod
+    @commands.register(args = [], result = [])
     def stop(self) -> Task[()]:
         """Immediately stop the current movement."""
         pass
 
     @abstractmethod
+    @commands.register(args = [], result = [])
     def free(self) -> Task[()]:
         """Immediately stop motor and go into freewheel."""
         pass
 
     @abstractmethod
+    @commands.register(args = [], result = [])
     def unfree(self) -> Task[()]:
         """Enable asservissement and keep robot in current position."""
         pass
@@ -89,15 +96,14 @@ class Pilot(Placable):
     def set_pose(self, pose: Pose2D) -> Task[()]:
         pass
 
-
-class DifferentialPilot(Pilot):
-    """
-    Interface to control the movement of a differential robot.
-    """
-
     @abstractmethod
+    @commands.register(args = [
+        ("x", ArgTypes.Float()),
+        ("y", ArgTypes.Float()),
+    ], result = [])
     def go_to(self, x: float, y: float) -> Task[PilotMoveStatus]:
         """Move to the given position. The returned Task can be cancelled."""
+        pass
 
     @abstractmethod
     def go_to_then_head_to(self, x: float, y: float, heading: float) -> Task[PilotMoveStatus]:
@@ -112,19 +118,40 @@ class DifferentialPilot(Pilot):
         pass
 
     @abstractmethod
-    def forward(self, distance: float) -> Task[PilotMoveStatus]:
-        pass
-
-    @abstractmethod
+    @commands.register(args = [
+        ("heading", ArgTypes.Float()),
+    ], result = [])
     def head_to(self, heading: float) -> Task[PilotMoveStatus]:
         pass
 
     @abstractmethod
+    @commands.register(args = [
+        ("x", ArgTypes.Float()),
+        ("y", ArgTypes.Float()),
+    ], result = [])
     def look_at(self, x: float, y: float) -> Task[PilotMoveStatus]:
         pass
 
     @abstractmethod
+    @commands.register(args = [
+        ("angle", ArgTypes.Float()),
+    ], result = [])
     def rotate(self, angle: float) -> Task[PilotMoveStatus]:
+        pass
+
+
+class DifferentialPilot(Pilot):
+    """
+    Interface to control the movement of a differential robot.
+    """
+
+    commands = DriverCommands([Pilot.commands])
+
+    @abstractmethod
+    @commands.register(args = [
+        ("forward", ArgTypes.Float()),
+    ], result = [])
+    def forward(self, distance: float) -> Task[PilotMoveStatus]:
         pass
 
     @abstractmethod
@@ -132,12 +159,14 @@ class DifferentialPilot(Pilot):
         pass
 
 
-class HolonomicPilot(DifferentialPilot):
+class HolonomicPilot(Pilot):
     """
     Interface to control the movement of a holonomic robot.
     It's extends the DifferentialPilot interface, so it has all the features of
     a differential robot plus some more.
     """
+
+    commands = DriverCommands([Pilot.commands])
 
     @abstractmethod
     def go_to_while_head_to(self, x: float, y: float, heading: float) -> Task[PilotMoveStatus]:
