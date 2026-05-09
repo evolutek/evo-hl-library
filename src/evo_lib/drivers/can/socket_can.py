62182c532be12eaf22ce37e6e90095ef4c6ef1f0
PAL <git@pal>
2026-05-10 00:10:47 +0200

    fix: Working socket can driver

--- src/evo_lib/drivers/can/socket_can.py
+++ src/evo_lib/drivers/can/socket_can.py
@@ -3,6 +3,7 @@ from queue import Empty, Queue
 
 import can
 
+from evo_lib.argtypes import ArgTypes
 from evo_lib.driver_definition import DriverDefinition, DriverInitArgs, DriverInitArgsDefinition
 from evo_lib.event import Event
 from evo_lib.interfaces.can import CAN, CANFilter, CANMessage
@@ -11,7 +12,7 @@ from evo_lib.task import ImmediateResultTask, Task
 
 
 class SocketCAN(CAN):
-    def __init__(self, name: str, logger: Logger, interface: str):
+    def __init__(self, name: str, logger: Logger, interface: str, extended: bool):
         super().__init__(name)
         self._interface = interface
         self._logger = logger
@@ -21,6 +22,7 @@ class SocketCAN(CAN):
         self._send_lock = threading.Lock()
         self._recv_messages = Queue[CANMessage]()
         self._recv_event = Event[CANMessage]()
+        self._extended = extended
 
     def init(self) -> Task[()]:
         self._bus = can.Bus(interface="socketcan", channel=self._interface)
@@ -34,6 +36,9 @@ class SocketCAN(CAN):
         if self._bus is not None:
             self._bus.shutdown()
             self._bus = None
+
+    def is_extended(self) -> bool:
+        return self._extended
 
     def _receiver_thread_func(self) -> None:
         while self._bus is not None:
@@ -58,7 +63,9 @@ class SocketCAN(CAN):
         assert self._bus is not None
         with self._send_lock:
             self._bus.send(
-                can.Message(arbitration_id=message.heading, data=message.data, is_extended_id=True)
+                can.Message(
+                    arbitration_id=message.heading, data=message.data, is_extended_id=self._extended
+                )
             )
 
     def write_async(self, message: CANMessage) -> Task[()]:
@@ -83,7 +90,7 @@ class SocketCAN(CAN):
         with self._send_lock:
             self._bus.set_filters(
                 [
-                    {"can_id": filter.id, "can_mask": filter.mask, "extended": True}
+                    {"can_id": filter.id, "can_mask": filter.mask, "extended": self._extended}
                     for filter in self._filters
                 ]
             )
@@ -101,14 +108,19 @@ class SocketCAN(CAN):
 
 class SocketCANDefininition(DriverDefinition):
     def __init__(self, logger: Logger) -> None:
+        super().__init__(CAN.commands)
         self._logger = logger
 
     def get_init_args_definition(self) -> DriverInitArgsDefinition:
-        return super().get_init_args_definition()
+        args = DriverInitArgsDefinition()
+        args.add_required("interface", ArgTypes.String())
+        args.add_required("extended", ArgTypes.Bool())
+        return args
 
     def create(self, args: DriverInitArgs) -> SocketCAN:
         return SocketCAN(
             name=args.get_name(),
             logger=self._logger,
             interface=args.get("interface"),
+            extended=args.get("extended"),
         )
